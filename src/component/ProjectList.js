3ad602306637096f2a5ac48f819e270d7d875767
Andrew Ellender <aelle14@lsu.edu>
2023-04-01 16:57:53 -0500

    Add projects

--- src/component/ProjectList.js
+++ src/component/ProjectList.js
@@ -1,6 +1,9 @@
 import Prog1 from "../assets/prog1.jpg";
 import Prog2 from "../assets/prog2.png";
 import Prog3 from "../assets/prog3.jpg";
+import Prog4 from "../assets/prog4.jpg";
+import Prog5 from "../assets/prog5.jpg";
+import Prog6 from "../assets/prog6.png";
 
 export const ProjectList = [
   {
@@ -20,5 +23,23 @@ export const ProjectList = [
     image: Prog3,
     skills: "Ecommerce website for buying and selling clothes. Accomplished using React.js, Node.js, AWS, MySQL database, and CSS.",
     link: "https://github.com/Drewe4401/Swappy",
+  },
+  {
+    name: "Realtime Chess Web App",
+    image: Prog4,
+    skills: "A responsive and real-time chess game built using React, Chessboardjsx, and Socket.io. Play chess with a friend online as the game board updates in real-time while you make your moves. The app detects checkmate and stalemate scenarios, declaring the winner or a draw accordingly.",
+    link: "https://github.com/Drewe4401/ChessGame",
+  },
+  {
+    name: "Movie Recommender System",
+    image: Prog5,
+    skills: "Movie Recommender System for personalized recommendations. Developed using Python, pandas, numpy, TuriCreate, IMDbPY, scikit-learn, matplotlib, seaborn, and nltk.",
+    link: "https://github.com/Drewe4401/Movie-Recommender-System",
+  },
+  {
+    name: "Arduino Whack-A-Mole Project",
+    image: Prog6,
+    skills: "A whack-a-mole game created using p5.js, p5.play.js, PDMSerial.js, and Tone.js, with interaction through an Arduino board.",
+    link: "https://github.com/Drewe4401/Whack-A-Mole-Arduino",
   }
 ];
